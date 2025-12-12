77ac26d62e0a56bad94d05f5f2ff5ae3881db28d
Chris Thomas <csthomas92@gmail.com>
2025-12-12 18:19:31 +0000

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,4 +1,9 @@
+const navToggle = document.getElementById("navToggle");
+const navLinks = document.getElementById("navLinks");
 
+navToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
 
 const phrases = [
     "Elevate Your Fitness",
